11198c2c08491ab255e1e3841106802d2863b5ff
Abey Onalaja <abey.onalaja@gmail.com>
2016-04-03 02:57:48 +0100

    Add Network script to handle socket.io

---
+++ Assets/Scripts/Network.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using SocketIO;
+using System;
+
+public class Network : MonoBehaviour {
+
+    static SocketIOComponent socket;
+    
+	// Use this for initialization
+	void Start () {
+        socket = GetComponent<SocketIOComponent>();
+        socket.On("open", OnConnected);
+	}
+
+    private void OnConnected(SocketIOEvent e)
+    {
+        Debug.Log("connected");
+        socket.Emit("move");
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
