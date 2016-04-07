3d0f95df549e1f58c6cf76ca77194380aa7b3d59
Abey Onalaja <abey.onalaja@gmail.com>
2016-04-07 18:39:09 +0100

    Create a dictionary of connected players

--- Assets/Scripts/Network.cs
+++ Assets/Scripts/Network.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 using SocketIO;
 using System;
 
@@ -8,23 +8,30 @@ public class Network : MonoBehaviour {
     static SocketIOComponent socket;
     public GameObject playerPrefab;
     
+    Dictionary<string, GameObject>players;
 	// Use this for initialization
 	void Start () {
         socket = GetComponent<SocketIOComponent>();
         socket.On("open", OnConnected);
         socket.On("spawn", OnSpawned);
         socket.On("move", OnMove);
+        
+        players = new Dictionary<string, GameObject>();
 	}
 
     private void OnMove(SocketIOEvent e)
     {
-        Debug.Log("A Player is moving " + e.data);
+        Debug.Log("A Player is moving " + e.data    );
     }
 
     private void OnSpawned(SocketIOEvent e)
     {
-        Debug.Log("spawned");
-        Instantiate(playerPrefab);
+        // Debug.Log("spawned player " + e.data);
+        var player = Instantiate(playerPrefab);
+        
+        players.Add(e.data["id"].ToString(), player);
+        
+        Debug.Log("count: " + players.Count);
     }
 
     private void OnConnected(SocketIOEvent e)
